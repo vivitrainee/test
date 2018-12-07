ee031ad76b74d09b8fe722a496b762cc191aff7b
yxhawk <yx_1203@163.com>
2018-12-07 11:00:58 +0800

    test

--- MaxPoint.lua
+++ MaxPoint.lua
@@ -1,3 +1,4 @@
+
 --[[
     @param points 点数组
     @ret 返回maxPoint
@@ -24,30 +25,50 @@ function maxPoints(points)
     local ret = 2
     for i=1,length do
         local samePos=0   --记录重复点
-        local sameSlope = 1 --斜率相同的点，第一个点
+        local verticalPos = 0 --垂直点
+        local map = {}
         for j=i + 1,length do
-            local positionX = points[i].x-points[j].x
-            local positionY = points[i].y-points[j].y
+            local positionX = points[i].x - points[j].x
+            local positionY = points[i].y - points[j].y
+            --print("j====",positionX, positionY ,  j)
             if positionX == 0 and positionY == 0 then     --重复
                 samePos = samePos + 1
+                print("samePos --= ",samePos, i, j)
             else --斜率
-				sameSlope = sameSlope + 1
-				for  k=j+1,length do
-					local positionX2 = points[k].x - points[i].x;
-					local positionY2 = points[k].y - points[i].y;
-					if positionX*positionY2 == positionY*positionX2 then
-						sameSlope = sameSlope + 1
-					end
-				end
+                --k = (y2-y1)/(x2-x1)
+
+                if points[i].x == points[j].x then --垂直
+                    verticalPos = verticalPos + 1
+                else
+                    local k = positionY / positionX
+                    if map[k] == nil then map[k] = 0 end
+                    map[k] = map[k] + 1
+                end
+
+                --k值最大
+                local maxK = 0
+                for k,v in pairs(map) do
+                    if maxK < v then
+                        maxK = v
+                    end
+                end
+
+                --垂直点和斜率相同点对比
+                local tmp = maxK
+                if tmp < verticalPos then
+                    tmp = verticalPos 
+                end
+
+                --求最大值
+                if ret < tmp then
+                    ret = tmp + samePos
+                else
+                    ret = ret + samePos
+                end
+
 
             end
         end
-
-        local tmp = samePos + sameSlope
-        if ret < tmp then
-            ret = tmp
-        end
-        sameSlope = 1
     end
 
     return ret
@@ -56,9 +77,9 @@ end
 
 local test = { 
     {x = 1, y = 3}, 
-    {x = 1, y = 3},
-    {x = 1, y = 3},
-    {x = 1, y = 3}, 
+    {x = 8, y = 2},
+    {x = 7, y = 4},
+    {x = 2, y = 5}, 
     {x = 1, y = 3}
 }
 
